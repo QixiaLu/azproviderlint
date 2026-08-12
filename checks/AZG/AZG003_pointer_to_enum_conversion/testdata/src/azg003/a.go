cf434f96ee25c13547a5d98e9fcfc5844d951730
Lucia Lu <qixialu@hotmail.com>
2026-08-12 11:16:30 +1000

    fix test

--- checks/AZG/AZG003_pointer_to_enum_conversion/testdata/src/azg003/a.go
+++ checks/AZG/AZG003_pointer_to_enum_conversion/testdata/src/azg003/a.go
@@ -76,21 +76,21 @@ type osTypeAlias = virtualmachines.OperatingSystemTypes
 
 // Should be flagged: the enum is referenced through a type alias.
 func invalidAliasedEnum() *virtualmachines.OperatingSystemTypes {
-	return pointer.To(osTypeAlias("Linux")) // want `pointer\.To with an explicit go-azure-sdk enum conversion should use pointer\.ToEnum\[OperatingSystemTypes\] instead`
+	return pointer.To(osTypeAlias("Linux")) // want `pointer\.To with an explicit go-azure-sdk enum conversion should use pointer\.ToEnum\[OperatingSystemTypes\]\(\.\.\.\) instead`
 }
 
 // Should be flagged: pointer.To with an explicit enum conversion of a variable.
 func invalidPriority() *virtualmachines.VirtualMachinePriorityTypes {
 	priority := "Spot"
-	return pointer.To(virtualmachines.VirtualMachinePriorityTypes(priority)) // want `pointer\.To with an explicit go-azure-sdk enum conversion should use pointer\.ToEnum\[VirtualMachinePriorityTypes\] instead`
+	return pointer.To(virtualmachines.VirtualMachinePriorityTypes(priority)) // want `pointer\.To with an explicit go-azure-sdk enum conversion should use pointer\.ToEnum\[VirtualMachinePriorityTypes\]\(\.\.\.\) instead`
 }
 
 // Should be flagged: pointer.To with an explicit enum conversion of a literal.
 func invalidOSType() *virtualmachines.OperatingSystemTypes {
-	return pointer.To(virtualmachines.OperatingSystemTypes("Linux")) // want `pointer\.To with an explicit go-azure-sdk enum conversion should use pointer\.ToEnum\[OperatingSystemTypes\] instead`
+	return pointer.To(virtualmachines.OperatingSystemTypes("Linux")) // want `pointer\.To with an explicit go-azure-sdk enum conversion should use pointer\.ToEnum\[OperatingSystemTypes\]\(\.\.\.\) instead`
 }
 
 // Should be flagged: pointer.To with an explicit enum conversion of a map lookup.
 func invalidFromMap(config map[string]interface{}) *virtualmachines.VirtualMachinePriorityTypes {
-	return pointer.To(virtualmachines.VirtualMachinePriorityTypes(config["priority"].(string))) // want `pointer\.To with an explicit go-azure-sdk enum conversion should use pointer\.ToEnum\[VirtualMachinePriorityTypes\] instead`
+	return pointer.To(virtualmachines.VirtualMachinePriorityTypes(config["priority"].(string))) // want `pointer\.To with an explicit go-azure-sdk enum conversion should use pointer\.ToEnum\[VirtualMachinePriorityTypes\]\(\.\.\.\) instead`
 }
